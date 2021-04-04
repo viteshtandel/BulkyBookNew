9b963db485777c6e841bf4cf2d90b5335650f567
Vitesh Tandel <viteshm.tandel@gmail.com>
2021-04-04 09:02:16 +0100

    first commit

---
+++ BulkyBook.Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [MaxLength(50)]
+        [Required]
+        [Display(Name="Category Name")]
+        public string Name { get; set; }
+    }
+}
